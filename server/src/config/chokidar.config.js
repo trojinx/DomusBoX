9ab116dfefaf645fe2085757bffbbe4d07d51fc6
shaaradnj <shaaradjarandikar@gmail.com>
2025-09-29 13:51:17 +0530

    emit event, when a file is uploaded

--- server/src/config/chokidar.config.js
+++ server/src/config/chokidar.config.js
@@ -1,3 +1,5 @@
+// apparently you can't emit an event from 1 module and hear it in another module, event both events are in same node process, LOL
+import eventBus from "./eventBus.config.js";
 import path from "path";
 import { fileURLToPath } from "url";
 import chokidar from "chokidar";
@@ -9,5 +11,9 @@ const uploadDir = path.join(__dirname, "../../uploads");
 console.log(typeof uploadDir);
 
 const watcher = chokidar.watch(`${uploadDir}`, { persistent: true });
-// TODO: Trigger event if file is uploaded
+watcher.on("add", (newFilePath) => {
+  eventBus.emit("fileUpload", newFilePath);
+  console.log(`newFilePath is: ${newFilePath}`);
+});
+
 export default watcher;
