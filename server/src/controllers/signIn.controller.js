3345fc26f4a67dd6fc5d2acb3f579b072280cf0a
trojinx <shaaradjarandikar@gmail.com>
2025-10-18 22:12:56 +0530

    add signin controller

---
+++ server/src/controllers/signIn.controller.js
@@ -0,0 +1,37 @@
+import User from "../schema/user.schema.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+async function signIn(req, res) {
+  try {
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email: email });
+    if (!existingUser) {
+      return res.status(404).send("Username not registered");
+    } else if (existingUser) {
+      const passwordIsCorrect = await bcrypt.compare(
+        password,
+        existingUser.hashedPassword
+      );
+      if (passwordIsCorrect) {
+        const bearerToken = jwt.sign(
+          {
+            username: existingUser.username,
+            email: email,
+          },
+          process.env.JWT_SECRET
+        );
+        return res.status(200).json({
+          bearerToken: bearerToken,
+          message: "Sign in successfull",
+        });
+      } else {
+        return res.status(400).send("Password is incorrect");
+      }
+    }
+  } catch (e) {
+    console.log(`error in signIn.controller.js while trying to sign in: ${e}`);
+    return res.status(500).send("Internal server error");
+  }
+}
+export default signIn;
