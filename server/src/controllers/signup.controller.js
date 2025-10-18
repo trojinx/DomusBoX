016669f911ec54fa7830f5d6169996fc51754bc4
trojinx <shaaradjarandikar@gmail.com>
2025-10-18 19:49:06 +0530

    add signup...

--- server/src/controllers/signup.controller.js
+++ server/src/controllers/signup.controller.js
@@ -1,2 +1,38 @@
-// username, password
-// this controller is there to register a device, and not the user itself
+import User from "../schema/user.schema.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+async function signUp(req, res) {
+  try {
+    const { username, email, password } = req.body;
+    const existingUser = await User.findOne({
+      $or: [{ email: email }, { username: username }],
+    });
+    if (existingUser) {
+      if (existingUser.username == username) {
+        return res.status(400).send("Username already taken");
+      } else if (existingUser.email == email) {
+        return res.status(400).send("Email already in use");
+      }
+    } else if (!existingUser) {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const newUser = new User({
+        username: username,
+        email: email,
+        hashedPassword: hashedPassword,
+      });
+      const bearerToken = await jwt.sign(
+        { username: username, email: email },
+        process.env.JWT_SECRET
+      );
+      await newUser.save();
+      return res.status(200).json({
+        bearerToken: bearerToken,
+        message: "User added successfully",
+      });
+    }
+  } catch (e) {
+    console.log(`error in signup.controller.js while trying to sign up: ${e}`);
+    return res.status(500).send("Internal server error");
+  }
+}
+export default signUp;
