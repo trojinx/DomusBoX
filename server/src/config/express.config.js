d901fe5e287a82864a97d8983bcd9de17f87ea81
trojinx <shaaradjarandikar@gmail.com>
2025-10-23 09:27:58 +0530

    minor change

--- server/src/config/express.config.js
+++ server/src/config/express.config.js
@@ -1,4 +1,14 @@
 import express from "express";
+import cors from "cors";
+
 const app = express();
+
+// Enable CORS for all routes
+app.use(cors({
+  origin: "*", // Allow all origins for development
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 app.use(express.json());
 export default app;
