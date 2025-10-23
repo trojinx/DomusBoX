3bd92d78ba6b1f196dfc89a26d7c19f62be14271
trojinx <shaaradjarandikar@gmail.com>
2025-10-23 09:28:14 +0530

    minor changes

--- server/server.js
+++ server/server.js
@@ -6,20 +6,28 @@ import letDownloadFile from "./src/controllers/letDownloadFile.controller.js";
 import uploadFileToServer from "./src/controllers/uploadFile.controller.js";
 import storage from "./src/config/multer.config.js";
 import keepAliveConnection from "./src/controllers/clientConnection.controller.js";
+import connectDB from "./src/config/db.config.js";
+import signUp from "./src/controllers/signup.controller.js";
+import signIn from "./src/controllers/signIn.controller.js";
+import verifyJWT from "./src/middleware/verifyJWT.middleware.js";
 const upload = multer({ storage });
 
 dotenv.config();
+connectDB();
 
 app.get("/testing", (req, res) => {
   res.status(200).send("testing success");
 });
 
+app.post("/signUp", signUp);
+app.post("/signIn", signIn);
+
 app.post("/fileUpload", upload.single("file"), uploadFileToServer);
 
-app.get("/download", letDownloadFile);
+app.get("/download", verifyJWT, letDownloadFile);
 
 app.get("/notifications", keepAliveConnection);
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, "0.0.0.0", () => {
   console.log(`server started on port: ${process.env.PORT}`);
 });
