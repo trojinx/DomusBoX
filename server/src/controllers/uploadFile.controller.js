e58bbd205251180dd6c1716b24d34ff0e4412339
shaaradnj <shaaradjarandikar@gmail.com>
2025-10-29 00:20:41 +0530

    add remove file logic if user is not validated/exist

--- server/src/controllers/uploadFile.controller.js
+++ server/src/controllers/uploadFile.controller.js
@@ -1,24 +1,49 @@
-async function uploadFileToServer(req, res) {
+// this file validates if receiving user exists for the given request. At this point, file is uploaded to server via multer,
+// just delete the file, if user does not exists, or password is incorrect.
+
+// This does not stop user from uploading wrong file, but deletes a file, if user does not exist, or password is incorrect,
+// to do this check beofre file upload, i need to use multer memory storage which i will do it later
+
+// TODO: think of some standart approach, this is a temp workaround, or maybe not!
+
+import fs from "node:fs/promises";
+import { fileURLToPath } from "node:url";
+import User from "../schema/user.schema.js";
+import bcrypt from "bcrypt";
+import path from "node:path";
+async function verifyUserCredentials(req, res) {
   try {
-    console.log("req.file:", req.file);
-    console.log("req.body:", req.body);
+    const { username, password } = req.body;
+    const existingUser = await User.findOne({ username: username });
+    if (!existingUser) {
+      return res.status(404).send("User with this username does not exist!");
+    } else if (existingUser) {
+      const passwordIsCorrect = await bcrypt.compare(
+        password,
+        existingUser.hashedPassword
+      );
 
-    if (!req.file) {
-      return res.status(400).send("No file uploaded");
+      if (!passwordIsCorrect) {
+        const currentPath = fileURLToPath(import.meta.url);
+        const fileName = `${username}_${req.file.originalname}`;
+        const fileRemovalPath = path.join(
+          `${currentPath}`,
+          "../../..",
+          "/uploads",
+          `${fileName}`
+        );
+        // console.log(fileRemovalPath);
+        fs.rm(fileRemovalPath);
+
+        return res
+          .status(401)
+          .send("Password is not correct, please try again");
+      } else {
+        return res.status(200).send("File uploaded successfully");
+      }
     }
-
-    res.status(200).send("File uploaded successfully");
   } catch (e) {
-    console.error("Error while writing file:", e);
-    res.status(500).send("Failed to upload file");
+    console.log(e);
   }
 }
-
-export default uploadFileToServer;
-
-// file upload --> done
-// file download --> done
-// electron app to automatically save the file --> remaining
-// signup, signin --> remaining
-// main thing: maintain keep alive connection with electron client
-// send notifications as soon as there is a file upload for him
+export default verifyUserCredentials;
