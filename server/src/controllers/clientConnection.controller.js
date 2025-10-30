6ab8c9ca4d2e9588e59eb018f79df624b4b3a995
shaaradnj <shaaradjarandikar@gmail.com>
2025-10-31 05:01:06 +0530

    minor change

--- server/src/controllers/clientConnection.controller.js
+++ server/src/controllers/clientConnection.controller.js
@@ -2,14 +2,12 @@ import eventBus from "../config/eventBus.config.js";
 
 async function keepAliveConnection(req, res) {
   try {
-    // const id = req.user.id;
     res.setHeader("Content-Type", "text/event-stream");
     res.setHeader("Connection", "keep-alive");
     res.setHeader("Cache-Control", "no-cache");
     res.flushHeaders();
     res.write("data: Connected to server, ready to receive notifications\n\n");
     eventBus.on("fileUpload", (newFilePath) => {
-      console.log(`wow, event watched and received`);
       const fileName = newFilePath.split("/")[1];
       res.write(`data: new file ready to receive: ${fileName}\n\n`);
     });
