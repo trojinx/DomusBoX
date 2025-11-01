3b2956c30a890617c19141b827b5872fafa15103
trojinx <shaaradjarandikar@gmail.com>
2025-11-02 01:20:52 +0530

    minor changes

--- server/src/controllers/clientConnection.controller.js
+++ server/src/controllers/clientConnection.controller.js
@@ -2,6 +2,11 @@ import eventBus from "../config/eventBus.config.js";
 
 async function keepAliveConnection(req, res) {
   try {
+    const username = req.user.username;
+    if (!username) {
+      return res.status(403).send("Please send JWT token");
+    }
+
     res.setHeader("Content-Type", "text/event-stream");
     res.setHeader("Connection", "keep-alive");
     res.setHeader("Cache-Control", "no-cache");
@@ -9,7 +14,10 @@ async function keepAliveConnection(req, res) {
     res.write("data: Connected to server, ready to receive notifications\n\n");
     eventBus.on("fileUpload", (newFilePath) => {
       const fileName = newFilePath.split("/")[1];
-      res.write(`data: new file ready to receive: ${fileName}\n\n`);
+      const ownerUsername = fileName.split("_")[0];
+      if (ownerUsername == username) {
+        res.write(`data: new file ready to receive: ${fileName}\n\n`);
+      }
     });
   } catch (e) {
     console.log(`error in clientConnection.controller.js: ${e}`);
