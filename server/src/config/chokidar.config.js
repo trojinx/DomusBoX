5c8d2765ecacc8302a4c12c9b85e7e109b208455
trojinx <shaaradjarandikar@gmail.com>
2025-11-02 01:24:16 +0530

    comment some code

--- server/src/config/chokidar.config.js
+++ server/src/config/chokidar.config.js
@@ -4,6 +4,8 @@ import path from "path";
 import { fileURLToPath } from "url";
 import chokidar from "chokidar";
 
+console.log(`chokidar is now watching!`);
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -12,10 +14,10 @@ const uploadDir = path.join(__dirname, "../../uploads");
 const watcher = chokidar.watch(`${uploadDir}`, { persistent: true });
 
 watcher.on("add", (newFilePath) => {
-  console.log(newFilePath);
+  // console.log(newFilePath);
 
   const fileNameOnly = newFilePath.split("uploads")[1];
-  console.log(fileNameOnly);
+  // console.log(fileNameOnly);
 
   eventBus.emit("fileUpload", fileNameOnly);
 });
