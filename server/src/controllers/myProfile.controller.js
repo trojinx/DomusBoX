e9c427ddfaac607da5ad94c39f72762defe5b3a9
trojinx <shaaradjarandikar@gmail.com>
2025-12-13 18:38:43 +0530

    merged file history in the same file

--- server/src/controllers/myProfile.controller.js
+++ server/src/controllers/myProfile.controller.js
@@ -1,4 +1,5 @@
 import User from "../schema/user.schema.js";
+import File from "../schema/fileTransaction.schema.js";
 async function showProfileInfo(req, res) {
   try {
     const id = req.user.id;
@@ -15,4 +16,19 @@ async function showProfileInfo(req, res) {
     return res.status(500).send("Internal server error");
   }
 }
-export default showProfileInfo;
+
+async function showFileHistory(req, res) {
+  try {
+    const userID = req.user.id;
+    const foundFiles = await File.find({ fileOwnerID: userID });
+    if (foundFiles.length == 0) {
+      return res.status(404).send("No previous files");
+    } else if (foundFiles.length >= 1) {
+      return res.status(200).json({ foundFiles: foundFiles });
+    }
+  } catch (e) {
+    console.log(`error in fileHistory.controller.js: ${e}`);
+    return res.status(500).send("Internal server error");
+  }
+}
+export { showProfileInfo, showFileHistory };
