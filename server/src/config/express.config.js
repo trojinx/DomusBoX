279fe79bc9863b3ebdf5df0d2e0f55b71477c59f
trojinx <shaaradjarandikar@gmail.com>
2025-12-13 18:47:14 +0530

    add express-rate-limit config

--- server/src/config/express.config.js
+++ server/src/config/express.config.js
@@ -1,14 +1,27 @@
 import express from "express";
 import cors from "cors";
+import { rateLimit } from "express-rate-limit";
 
 const app = express();
+app.set("trust proxy", 1);
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+  // store: ... , // Redis, Memcached, etc. See below.
+});
 
 // Enable CORS for all routes
-app.use(cors({
-  origin: "*", // Allow all origins for development
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
+app.use(
+  cors({
+    origin: "*", // Allow all origins for development
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
+app.use(limiter);
 export default app;
