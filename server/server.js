dd309d29e224f0dcd50cae0130cf40f36703a90f
trojinx <shaaradjarandikar@gmail.com>
2025-12-13 18:37:01 +0530

    comment, update

--- server/server.js
+++ server/server.js
@@ -3,25 +3,29 @@ import dotenv from "dotenv";
 import multer from "multer";
 import letDownloadFile from "./src/controllers/letDownloadFile.controller.js";
 import watcher from "./src/config/chokidar.config.js"; // not importing this causes chokidar watcher to not exec, so keep it imported
-import uploadFileToServer from "./src/controllers/uploadFile.controller.js";
+// import uploadFileToServer from "./src/controllers/uploadFile.controller.js";
 import storage from "./src/config/multer.config.js";
 import keepAliveConnection from "./src/controllers/clientConnection.controller.js";
 import connectDB from "./src/config/db.config.js";
 import signUp from "./src/controllers/signupDeskApp.controller.js";
-// import signIn from "./src/controllers/signInDeskApp.controller.js";
 import verifyJWT from "./src/middleware/verifyJWT.middleware.js";
 import recordFileDownload from "./src/controllers/recordFileDownload.controller.js";
-import showFileHistory from "./src/controllers/fileHistory.controller.js";
-import showProfileInfo from "./src/controllers/myProfile.controller.js";
+// import showFileHistory from "./src/controllers/fileHistory.controller.js";
+import {
+  showProfileInfo,
+  showFileHistory,
+} from "./src/controllers/myProfile.controller.js";
 import signInWithJWT from "./src/controllers/signInWithJWTDeskApp.controller.js";
-import deleteFile from "./src/controllers/deleteFile.controller.js";
+// import deleteFile from "./src/controllers/deleteFile.controller.js";
 import signInWeb from "./src/controllers/signInWeb.controller.js";
 import signInDesk from "./src/controllers/signInDeskApp.controller.js";
 import respond200 from "./src/controllers/respond200.controller.js";
+
 const upload = multer({ storage });
 
 dotenv.config();
 connectDB();
+// deleteFile();
 
 app.get("/testing", (req, res) => {
   res.status(200).send("testing success");
